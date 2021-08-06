de9c36f4332d7f08c864babecc6377dbc7ec33df
saurabhs <saurabhpshirsath@gmail.com>
2021-08-06 21:09:45 +0530

    [saurabh] Add :Insert element After Key Value in LinkedList

--- List/Program.cs
+++ List/Program.cs
@@ -11,6 +11,10 @@ namespace List
             linkedList.Add(30);
             linkedList.Add(70);
             linkedList.Display();
+            Console.WriteLine("-----------------");
+            int value = linkedList.Search(30);
+            linkedList.InsertAtParticularPosition(value + 1, 40);
+            linkedList.Display();
         }
     }
 }
